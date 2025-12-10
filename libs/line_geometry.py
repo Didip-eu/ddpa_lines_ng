Fix bug in scaling from baseline.

libs/line_geometry.py
@@ -313,19 +313,20 @@ def binary_mask_from_fixed_patches( img: Image.Image, patch_size=1024, overlap=.
     return page_mask[None,:]
 
 
-def strip_from_baseline(baseline_n2xy: list[tuple[int,int]], height: float, ltrb: tuple[int,int,int,int]=tuple()) -> list[tuple[int,int]]:
+def strip_from_baseline(baseline_n2xy: list[tuple[int,int]], x_height: int, factor: float, ltrb: tuple[int,int,int,int]=tuple()) -> list[tuple[int,int]]:
     """
     Given a baseline, construct the strip-shaped polygon with given height.
 
     Args:
         baseline_n2xy (list[tuple[int,int]]): a sequence of (x,y) points.
-        height (float): the strip height.
+        x_height (float): the line x_height.
+        factor (float): scaling factor
         ltrb (tuple[int,int,int,int]): LTRB constraint of containing region: if not empty (default),
             shift coordinates that would otherwise exceed the region's boundaries.
     Returns:
         list[tuple[int,int]]: a (N,2) clockwise sequence of (x,y) points.
     """
-    raw_polygon = strip_from_centerline( np.array( baseline_n2xy )-[0,height/2], height )
+    raw_polygon = strip_from_centerline( np.array( baseline_n2xy )-[0,x_height/2], int(x_height*factor) )
     if ltrb:
         return boxed_in( raw_polygon, ltrb ).tolist()
     return raw_polygon.tolist()

libs/seglib.py
@@ -209,7 +209,7 @@ def line_polygons_from_segmentation_dict( segmentation_dict: dict, polygon_key='
         for line in segmentation_dict['lines']:
             if 'x-height' in line:
                 ltrb = tuple(np.array(line['regions'][0]['coords'])[[0,2]].flatten())
-                line_polygons.append( lgm.strip_from_baseline( line['baseline'], line['x-height']*factor, ltrb=ltrb) )
+                line_polygons.append( lgm.strip_from_baseline( line['baseline'], line['x-height'], factor, ltrb=ltrb) )
             else:
                 line_polygons.append( line[polygon_key] )
     elif 'regions' in segmentation_dict:
@@ -218,7 +218,7 @@ def line_polygons_from_segmentation_dict( segmentation_dict: dict, polygon_key='
             return [ line[polygon_key] for reg in segmentation_dict['regions'] for line in reg['lines']] 
         for reg in segmentation_dict['regions']:
             ltrb=tuple(np.array(reg['coords'])[[0,2]].flatten())
-            line_polygons.extend([ lgm.strip_from_baseline( line['baseline'], line['x-height']*factor, ltrb=ltrb ) if 'x-height' in line else line[polygon_key] for line in reg['lines'] ] )
+            line_polygons.extend([ lgm.strip_from_baseline( line['baseline'], line['x-height'], factor, ltrb=ltrb ) if 'x-height' in line else line[polygon_key] for line in reg['lines'] ] )
     return line_polygons
     
 