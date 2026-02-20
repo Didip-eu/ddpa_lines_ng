10a35f89384cbc5cc419400f47e805d5ec07e8bb
Nicolas Renet <nprenet@gmail.com>
2026-02-20 16:41:41 +0100

    Grid training script.

--- bin/train_grid.sh
+++ bin/train_grid.sh
@@ -4,18 +4,20 @@
 #
 scheduler_factors=".9 .8 .7"
 scheduler_patiences="6 8 4"
+backbones="resnet101 resnet50"
 learning_rates=".0001 .0002 .0005 .00075"
-backbones="resnet50 resnet101"
 
 export PYTHONPATH=.
 for sf in $scheduler_factors; do
 	for sp in $scheduler_patiences; do
 		for lr in $learning_rates; do
 			for bb in $backbones; do
-				saved_model=models/best.$(date +"%m%d%Y").lr${lr:1}.bb${bb}.sf${sf:1}.sp${sp}.mlmodel
+				#saved_model=models/best.$(date +"%m%d%Y").lr${lr:1}.bb${bb}.sf${sf:1}.sp${sp}.mlmodel
+				saved_model=models/best.lr${lr:1}.bb${bb}.sf${sf:1}.sp${sp}.mlmodel
 				test -f "${saved_model}" && continue
 				./bin/ddp_lineseg_train.py -lr ${lr} -backbone ${bb} -scheduler_factor ${sf} -cache_dir dataset/cached -max_epoch 60 -scheduler 1 -scheduler_patience ${sp} -scheduler_cooldown 2 -patience 20 -max_epoch 100 -device cuda -batch_size 6
-				test -f best.mlmodel && mv best.mlmodel models/best.$(date +"%m%d%Y").lr${lr:1}.bb${bb}.sf${sf:1}.sp${sp}.mlmodel
+				#test -f best.mlmodel && mv best.mlmodel models/best.$(date +"%m%d%Y").lr${lr:1}.bb${bb}.sf${sf:1}.sp${sp}.mlmodel
+				test -f best.mlmodel && mv best.mlmodel $saved_model
 				rm -f last.*
 			done
 		done
